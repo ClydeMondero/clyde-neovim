06eea5dbc01fd64ef04ad27c006e51fa8ec6d260
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2024-01-21 17:54:30 +0800

    refactor: reverted neotree to float

--- lua/neotree-setup.lua
+++ lua/neotree-setup.lua
@@ -1,7 +1,7 @@
 require('neo-tree').setup({
   cmd = 'Neotree',
   window = {
-    position = "left"
+    position = "float"
   },
   filesystem = {
     filtered_items = {
@@ -10,18 +10,15 @@ require('neo-tree').setup({
   },
   init = function()
     vim.api.nvim_create_autocmd('BufEnter', {
-      -- make a group to be able to delete it later
       group = vim.api.nvim_create_autogroup('NeoTreeInit', { clear = true }),
       callback = function()
         local f = vim.fn.expand('%:p')
         if vim.fn.isdirectory(f) ~= 0 then
           vim.cmd('Neotree current dir=' .. f)
-          -- neo-tree is loaded now, delete the init autocmd
           vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
         end
       end
     })
-    -- keymaps
   end,
   opts = {
     filesystem = {
